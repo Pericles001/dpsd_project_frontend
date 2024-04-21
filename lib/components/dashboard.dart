fbe27a2f17966c672eb052a79051b48057b2b96e
Pericles001 <periclesadjovi@gmail.com>
2024-04-21 19:08:27 +0200

    Update file content

--- lib/components/dashboard.dart
+++ lib/components/dashboard.dart
@@ -7,7 +7,7 @@ class DashboardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard'),
+        title: const Text('Dashboard'),
       ),
       body: Center(
         child: Column(
@@ -18,14 +18,14 @@ class DashboardPage extends StatelessWidget {
               onPressed: () {
                 // Implement functionality for dashboard button 1
               },
-              child: Text('Dashboard Button 1'),
+              child: const Text('Dashboard Button 1'),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 // Implement functionality for dashboard button 2
               },
-              child: Text('Dashboard Button 2'),
+              child: const Text('Dashboard Button 2'),
             ),
             // Add more dashboard elements as needed
           ],
