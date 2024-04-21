83014ab78226ec14d1cf3b74cb42123564c9301a
Pericles001 <periclesadjovi@gmail.com>
2024-04-21 19:10:49 +0200

    Edit register.dart: - Add call for api_service.dart methods

--- lib/auth/register.dart
+++ lib/auth/register.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import '../api/api_service.dart';
 import 'login.dart'; // Make sure to import the LoginPage
 
 class RegisterPage extends StatelessWidget {
@@ -6,10 +8,14 @@ class RegisterPage extends StatelessWidget {
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController = TextEditingController();
   final TextEditingController _roleController = TextEditingController();
+
+  final ApiService _apiService = ApiService();
 
   @override
   Widget build(BuildContext context) {
@@ -30,14 +36,26 @@ class RegisterPage extends StatelessWidget {
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
-                      TextFormField(
-                        controller: _usernameController,
+                       TextFormField(
+                        controller: _firstNameController,
                         decoration: const InputDecoration(
-                          labelText: 'Username',
+                          labelText: 'First Name',
                         ),
                         validator: (value) {
                           if (value == null || value.isEmpty) {
-                            return 'Please enter your username';
+                            return 'Please enter your first name';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        controller: _lastNameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Last Name',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your last name';
                           }
                           return null;
                         },
@@ -113,9 +131,19 @@ class RegisterPage extends StatelessWidget {
                       ),
                       const SizedBox(height: 20),
                       ElevatedButton(
-                        onPressed: () {
+                        onPressed: () async {
                           if (_formKey.currentState!.validate()) {
-                            // Process data.
+                            final response = await _apiService.registerUser(
+                              _firstNameController.text,
+                              _lastNameController.text,
+                              _emailController.text,
+                              _passwordController.text,
+                            );
+
+                            // Handle the response
+                            if (kDebugMode) {
+                              print(response);
+                            }
                           }
                         },
                         style: ElevatedButton.styleFrom(
@@ -140,142 +168,3 @@ class RegisterPage extends StatelessWidget {
   }
 }
 
-// import 'package:flutter/material.dart';
-// import 'login.dart'; // Make sure to import the LoginPage
-//
-// class RegisterPage extends StatelessWidget {
-//   RegisterPage({super.key});
-//
-//   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-//   final TextEditingController _usernameController = TextEditingController();
-//   final TextEditingController _emailController = TextEditingController();
-//   final TextEditingController _passwordController = TextEditingController();
-//   final TextEditingController _confirmPasswordController = TextEditingController();
-//   final TextEditingController _roleController = TextEditingController();
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Register'),
-//       ),
-//       body: Center(
-//         child: SizedBox(
-//           width: MediaQuery.of(context).size.width * 0.8, // 80% of screen width
-//           child: Card(
-//             elevation: 8.0,
-//             child: Padding(
-//               padding: const EdgeInsets.all(16.0),
-//               child: Form(
-//                 key: _formKey,
-//                 child: Column(
-//                   mainAxisSize: MainAxisSize.min,
-//                   children: <Widget>[
-//                     TextFormField(
-//                       controller: _usernameController,
-//                       decoration: const InputDecoration(
-//                         labelText: 'Username',
-//                       ),
-//                       validator: (value) {
-//                         if (value == null || value.isEmpty) {
-//                           return 'Please enter your username';
-//                         }
-//                         return null;
-//                       },
-//                     ),
-//                     TextFormField(
-//                       controller: _roleController,
-//                       decoration: const InputDecoration(
-//                         labelText: 'Role',
-//                       ),
-//                       validator: (value) {
-//                         if (value == null || value.isEmpty) {
-//                           return 'Please enter your role';
-//                         }
-//                         return null;
-//                       },
-//                     ),
-//                     TextFormField(
-//                       controller: _emailController,
-//                       decoration: const InputDecoration(
-//                         labelText: 'Email',
-//                       ),
-//                       validator: (value) {
-//                         if (value == null || value.isEmpty) {
-//                           return 'Please enter your email';
-//                         }
-//                         return null;
-//                       },
-//                     ),
-//                     TextFormField(
-//                       controller: _passwordController,
-//                       decoration: const InputDecoration(
-//                         labelText: 'Password',
-//                       ),
-//                       obscureText: true,
-//                       validator: (value) {
-//                         if (value == null || value.isEmpty) {
-//                           return 'Please enter your password';
-//                         }
-//                         return null;
-//                       },
-//                     ),
-//                     TextFormField(
-//                       controller: _confirmPasswordController,
-//                       decoration: const InputDecoration(
-//                         labelText: 'Confirm Password',
-//                       ),
-//                       obscureText: true,
-//                       validator: (value) {
-//                         if (value == null || value.isEmpty) {
-//                           return 'Please confirm your password';
-//                         }
-//                         if (value != _passwordController.text) {
-//                           return 'Passwords do not match';
-//                         }
-//                         return null;
-//                       },
-//                     ),
-//                     const SizedBox(height: 20),
-//                     GestureDetector(
-//                       onTap: () {
-//                         Navigator.push(
-//                           context,
-//                           MaterialPageRoute(builder: (context) => LoginPage()),
-//                         );
-//                       },
-//                       child: const Text(
-//                         'Already have an account? Login',
-//                         style: TextStyle(
-//                           color: Colors.blue,
-//                           decoration: TextDecoration.underline,
-//                         ),
-//                       ),
-//                     ),
-//                     const SizedBox(height: 20),
-//                     ElevatedButton(
-//                       onPressed: () {
-//                         if (_formKey.currentState!.validate()) {
-//                           // Process data.
-//                         }
-//                       },
-//                       style: ElevatedButton.styleFrom(
-//                         backgroundColor: Theme.of(context).colorScheme.secondary,
-//                         foregroundColor: Theme.of(context).colorScheme.onSecondary,
-//                         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-//                         shape: RoundedRectangleBorder(
-//                           borderRadius: BorderRadius.circular(4),
-//                         ),
-//                       ),
-//                       child: const Text('Register'),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
