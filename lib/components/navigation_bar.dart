fb63d757ef6c25f55be71c01b6530a5b7139aa54
Pericles001 <periclesadjovi@gmail.com>
2024-04-22 21:52:37 +0200

    Edit navigation bar component

--- lib/components/navigation_bar.dart
+++ lib/components/navigation_bar.dart
@@ -1,67 +1,86 @@
+import 'package:dpsd_project2_frontend_iteration_1/home/menu.dart';
 import 'package:flutter/material.dart';
+import '../features/alerts/index.dart';
 import '../features/faq/index.dart';
 import '../features/housing_ventilation/index.dart';
 
-class CustomNavigationBar extends StatelessWidget {
-  const CustomNavigationBar({
-    Key? key,
-  }) : super(key: key);
+
+class CustomNavigationBar extends StatefulWidget {
+  const CustomNavigationBar({super.key});
+
+  @override
+  _CustomNavigationBarState createState() => _CustomNavigationBarState();
+}
+
+class _CustomNavigationBarState extends State<CustomNavigationBar> {
+  final _pageController = PageController();
+  int _currentIndex = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: -1, // Set to -1 so no item is selected
-      selectedItemColor: Theme.of(context).colorScheme.secondary,
-      unselectedItemColor: Theme.of(context).colorScheme.onSurface,
-      showSelectedLabels: false, // Don't show labels for selected items
-      showUnselectedLabels: false, // Don't show labels for unselected items
-      onTap: (index) {
-        switch (index) {
-          case 0: // 'HomeMenu' item
-            return;
-          case 1: // 'Ambient' item
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const VentilationIndex()),
-            );
-            break;
-          case 2: // 'Alerts' item
-            // Implement navigation to Alerts page
-            break;
-          case 3: // 'FAQ' item
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const FAQIndex()),
-            );
-            break;
-          case 4: // 'Pigs Manager' item
-            // Implement navigation to Pigs Manager page
-            break;
-          // Add more conditions here for other items if needed
-        }
-      },
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'HomeMenu',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.cloud),
-          label: 'Ambient',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.warning),
-          label: 'Alerts',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.question_answer),
-          label: 'FAQ',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.pets),
-          label: 'Pigs Manager',
-        ),
-      ],
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        children: const [
+          HomeMenu(),
+          VentilationIndex(),
+          AlertIndex(),
+          FAQIndex(),
+          // Add your Pigs Manager page here
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        unselectedItemColor: Theme.of(context).colorScheme.onSurface,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeIn,
+          );
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'HomeMenu',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cloud),
+            label: 'Ambient',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.warning),
+            label: 'Alerts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.question_answer),
+            label: 'FAQ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pets),
+            label: 'Pigs Manager',
+          ),
+        ],
+      ),
     );
   }
 }
+
+
