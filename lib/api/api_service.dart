37b3ad2ac36ffee98df5d24221075f9bb1d1f1ea
Pericles001 <periclesadjovi@gmail.com>
2024-04-23 10:28:28 +0200

    Edit file: -Add code to fetch weather API data

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -11,6 +11,58 @@ class ApiService {
     http.Client();
   }
 
+  Future<String> registerTestUser() async {
+    var client = http.Client();
+    var headers = {
+      'Content-Type': 'application/json'
+    };
+    var request = http.Request('POST', Uri.parse('$_baseUrl/register'));
+    request.body = json.encode({
+      "firstName": "test",
+      "lastName": "test13",
+      "email": "test24d@test",
+      "password": "default"
+    });
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await client.send(request);
+
+    if (response.statusCode == 200) {
+      return await response.stream.bytesToString();
+    } else {
+      throw Exception('Failed to register test user: ${response.reasonPhrase}');
+    }
+  }
+  Future<Map<String, dynamic>> fetchWeatherData(String city) async {
+  var response = await http.get(Uri.parse(
+      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/$city?unitGroup=metric&key=LQ892399QVFZUUCELK66XYCSF&contentType=json'));
+
+  if (response.statusCode == 200) {
+    var data = jsonDecode(response.body);
+    var temperature = data['days'][0]['temp'];
+    var humidity = data['days'][0]['humidity'];
+    var precipitation = data['days'][0]['precip'];
+    var sun = {
+      'sunrise': data['days'][0]['sunrise'],
+      'sunset': data['days'][0]['sunset'],
+    };
+    var wind = {
+      'windgust': data['days'][0]['windgust'],
+      'windspeed': data['days'][0]['windspeed'],
+      'winddir': data['days'][0]['winddir'],
+    };
+    return {
+      'temperature': temperature,
+      'humidity': humidity,
+      'precipitation': precipitation,
+      'sun': sun,
+      'wind': wind,
+    };
+  } else {
+    throw Exception('Failed to load data');
+  }
+}
+
   Future<Map<String, dynamic>> registerUser(String firstName, String lastName, String email, String password) async {
     try {
       var headers = {
@@ -60,54 +112,5 @@ class ApiService {
       throw Exception('Request timed out. Please try again.');
     }
   }
-}
 
-// class ApiService {
-//   static const String _baseUrl = 'http://10.0.2.2:8080/api';
-//
-//   set client(MockClient client) {
-//     http.Client();
-//   }
-//
-//   Future<Map<String, dynamic>> registerUser(String firstName, String lastName, String email, String password) async {
-//     final response = await http.post(
-//       Uri.parse('$_baseUrl/register'),
-//       headers: <String, String>{
-//         'Content-Type': 'application/json; charset=UTF-8',
-//       },
-//       body: jsonEncode(<String, String>{
-//         'firstName': firstName,
-//         'lastName': lastName,
-//         'email': email,
-//         'password': password,
-//       }),
-//     );
-//
-//     if (response.statusCode == 200) {
-//       return jsonDecode(response.body);
-//     } else {
-//       throw Exception('Failed to register user');
-//     }
-//   }
-//
-//   Future<Map<String, dynamic>> loginUser(String email, String password) async {
-//     var headers = {
-//       'Content-Type': 'application/json'
-//     };
-//     var request = http.Request('POST', Uri.parse('$_baseUrl/login'));
-//     request.body = json.encode({
-//       "email": email,
-//       "password": password
-//     });
-//     request.headers.addAll(headers);
-//
-//     http.StreamedResponse response = await request.send();
-//
-//     if (response.statusCode == 200) {
-//       return jsonDecode(await response.stream.bytesToString());
-//     } else {
-//       throw Exception('Failed to login user: ${response.reasonPhrase}');
-//     }
-//   }
-// }
-//
+}
