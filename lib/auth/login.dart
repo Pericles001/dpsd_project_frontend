3ff07686ae3a5b9e0a9190121f3b24c879149e7c
Pericles001 <periclesadjovi@gmail.com>
2024-05-02 22:57:43 +0200

    Edit file: - Edit function to handle user login into application

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,15 +1,20 @@
+import 'dart:convert';
+
 import 'package:dpsd_project2_frontend_iteration_1/auth/register.dart';
 import 'package:dpsd_project2_frontend_iteration_1/auth/reset_password.dart';
-// Import the DashboardPage
 import 'package:dpsd_project2_frontend_iteration_1/home/menu.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:dpsd_project2_frontend_iteration_1/api/api_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
-
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _apiService = ApiService();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -90,15 +95,52 @@ class LoginPage extends StatelessWidget {
                     ),
                     const SizedBox(height: 20),
                     ElevatedButton(
-                      onPressed: () {
+                      onPressed: () async {
                         if (_formKey.currentState!.validate()) {
                           // Process login
-                          // If login is successful, navigate to the DashboardPage
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomeMenu()),
-                          );
+                          print('Submitting login form');
+                          try {
+                            var result = await _apiService.loginUser(
+                              _emailController.text,
+                              _passwordController.text,
+                            );
+                            // If login is successful, save user information and navigate to the DashboardPage
+                            if (kDebugMode) {
+                              print('Token: ${result['token']}');
+                            }
+                            SharedPreferences prefs =
+                                await SharedPreferences.getInstance();
+                            await prefs.setString('token', result['token']);
+                            await prefs.setString(
+                                'user', jsonEncode(result['user']));
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomeMenu()),
+                            );
+                          } catch (e) {
+                            // Handle login failure
+                            if (kDebugMode) {
+                              print('Login failed: $e');
+                            }
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Error'),
+                                  content: Text('Failed to login: $e'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      child: const Text('OK'),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
                         }
                       },
                       style: ElevatedButton.styleFrom(
