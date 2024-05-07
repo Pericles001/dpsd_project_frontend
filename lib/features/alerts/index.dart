54f1591d1f0a375a153e11ec87cdbaff1a592a96
Pericles001 <periclesadjovi@gmail.com>
2024-05-07 13:04:56 +0200

    Edit file: - Add logical checking for ambient variables

--- lib/features/alerts/index.dart
+++ lib/features/alerts/index.dart
@@ -4,6 +4,10 @@ import '../../auth/login.dart';
 import '../../home/menu.dart';
 import '../faq/index.dart';
 import '../housing_ventilation/index.dart';
+import '../../api/api_service.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import '../pig/index.dart';
 
 class AlertIndex extends StatefulWidget {
   const AlertIndex({super.key});
@@ -14,6 +18,101 @@ class AlertIndex extends StatefulWidget {
 
 class _AlertIndexState extends State<AlertIndex> {
   int _currentIndex = 0;
+  final ApiService _apiService =
+      ApiService(); // Create an instance of ApiService
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  // Define the actual values here
+  double? temperature;
+  double? humidity;
+  double? precipitation;
+
+  // Ambient variables
+  Map<String, double> ambientVariables = {};
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   fetchAndCompareWeatherData();
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    var initializationSettingsAndroid =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = const IOSInitializationSettings();
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    fetchAndCompareWeatherData();
+  }
+
+  Future<void> showNotification(String title, String body) async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        'alert_channel_id', 'Alerts', 'Notification for new alerts',
+        importance: Importance.max, priority: Priority.high, showWhen: false);
+    var iOSPlatformChannelSpecifics = const IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0, title, body, platformChannelSpecifics);
+  }
+
+  Future<void> fetchAndCompareWeatherData() async {
+    try {
+      final weatherData = await _apiService
+          .fetchWeatherData('kigali'); // Replace 'city' with the actual city
+      final ambientData = await _apiService.getAmbientVariables(
+          'token'); // Replace 'token' with the actual token
+
+      // Parse ambient variables
+      for (var variable in ambientData) {
+        ambientVariables[variable['name']] = variable['threshold'];
+      }
+
+      setState(() {
+        temperature = weatherData['temperature'];
+        humidity = weatherData['humidity'];
+        precipitation = weatherData['precipitation'];
+      });
+
+      // Check if the weather data exceeds the thresholds and show a notification if it does
+      if (temperature! > ambientVariables['temperature']!) {
+        await showNotification(
+            'Temperature Alert', 'The temperature is exceeding the threshold');
+      }
+      if (humidity! > ambientVariables['humidity']!) {
+        await showNotification(
+            'Humidity Alert', 'The humidity is exceeding the threshold');
+      }
+      if (precipitation! > ambientVariables['precipitation']!) {
+        await showNotification('Precipitation Alert',
+            'The precipitation is exceeding the threshold');
+      }
+    } catch (e) {
+      print('Error fetching weather data: $e');
+    }
+  }
+
+  Color getAlertColor(String variableName, double? value) {
+    double threshold = ambientVariables.containsKey(variableName)
+        ? ambientVariables[variableName]!
+        : 0; // Use a default threshold of 0 if the variable does not exist in the ambient variables
+
+    if (value == null) {
+      return Colors.grey;
+    } else if (value > threshold) {
+      return Colors.red;
+    } else if (value == threshold) {
+      return Colors.yellow;
+    } else {
+      return Colors.green;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,23 +121,97 @@ class _AlertIndexState extends State<AlertIndex> {
         title: const Text('Alerts'),
       ),
       body: ListView(
-        children: const <Widget>[
+        children: <Widget>[
           Card(
-            child: ListTile(
-              title: Text('Alert 1'),
-              subtitle: Text('Alert 1 description'),
+            color: getAlertColor('temperature', temperature),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('Temperature: ${temperature ?? 'Loading...'}'),
+                  trailing: ElevatedButton(
+                    child: const Text('Comments'),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return FutureBuilder<String>(
+                            future: _apiService.getPromptGemini(
+                                'Temperature is ${(temperature ?? 0) > (ambientVariables['temp'] ?? 0) ? 'exceeding' : 'lower than'} the threshold'),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return const AlertDialog(
+                                  content: SingleChildScrollView(
+                                    child: Text('Loading comment...'),
+                                  ),
+                                );
+                              } else if (snapshot.hasError) {
+                                return AlertDialog(
+                                  content: SingleChildScrollView(
+                                    child: Text('Error: ${snapshot.error}'),
+                                  ),
+                                );
+                              } else {
+                                return AlertDialog(
+                                  content: SingleChildScrollView(
+                                    child: Text('Comment: ${snapshot.data}'),
+                                  ),
+                                );
+                              }
+                            },
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Text('Alert 2'),
-              subtitle: Text('Alert 2 description'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('Alert 3'),
-              subtitle: Text('Alert 3 description'),
+            color: getAlertColor('humidity', humidity),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('Humidity: ${humidity ?? 'Loading...'}'),
+                  trailing: ElevatedButton(
+                    child: const Text('Comments'),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return FutureBuilder<String>(
+                            future: _apiService.getPromptGemini(
+                                'Humidity is ${(humidity ?? 0) > (ambientVariables['humidity'] ?? 0) ? 'exceeding' : 'lower than'} the threshold'),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return const AlertDialog(
+                                  content: SingleChildScrollView(
+                                    child: Text('Loading comment...'),
+                                  ),
+                                );
+                              } else if (snapshot.hasError) {
+                                return AlertDialog(
+                                  content: SingleChildScrollView(
+                                    child: Text('Error: ${snapshot.error}'),
+                                  ),
+                                );
+                              } else {
+                                return AlertDialog(
+                                  content: SingleChildScrollView(
+                                    child: Text('Comment: ${snapshot.data}'),
+                                  ),
+                                );
+                              }
+                            },
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ],
@@ -47,8 +220,8 @@ class _AlertIndexState extends State<AlertIndex> {
         currentIndex: _currentIndex,
         selectedItemColor: Theme.of(context).colorScheme.secondary,
         unselectedItemColor: Theme.of(context).colorScheme.onSurface,
-        showSelectedLabels: false, // Don't show labels for selected items
-        showUnselectedLabels: false, // Don't show labels for unselected items
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
         onTap: (index) {
           setState(() {
             _currentIndex = index;
@@ -57,8 +230,7 @@ class _AlertIndexState extends State<AlertIndex> {
             case 0:
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                    builder: (context) => const HomeMenu()),
+                MaterialPageRoute(builder: (context) => const HomeMenu()),
               );
               break;
             case 1:
@@ -71,25 +243,26 @@ class _AlertIndexState extends State<AlertIndex> {
             case 2:
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                    builder: (context) => const AlertIndex()),
+                MaterialPageRoute(builder: (context) => const AlertIndex()),
               );
               break;
             case 3:
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                    builder: (context) => const FAQIndex()),
+                MaterialPageRoute(builder: (context) => const FAQIndex()),
               );
               break;
             case 4:
-            // Add your Pigs Manager page here
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const PigManagerIndex()),
+              );
               break;
             case 5:
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                    builder: (context) => LoginPage()),
+                MaterialPageRoute(builder: (context) => LoginPage()),
               );
           }
         },
